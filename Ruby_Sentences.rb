e329eeb5f181127d4751bdf46cb4712f0483fd62
Eric Fithian <ef2413@columbia.edu>
2017-06-23 22:49:09 -0400

    Updated sentences exercise

--- Ruby_Sentences.rb
+++ Ruby_Sentences.rb
@@ -1,5 +1,9 @@
-puts "Hey there, tell me something interesting."
-reply=gets.chomp
-puts "What is your favorite word in '#{reply}'?"
-fav_word=gets.chomp
+array = []
+
+puts "Hi please write a sentence for me"
+  reply=gets.chomp
+  array << reply.split(" ")
+
+puts "What is your favorite word in #{reply}?"
+  fav_word=gets.chomp
 puts reply.index(fav_word)
